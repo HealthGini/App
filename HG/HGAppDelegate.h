79df13f07e26fc3e90bd3641711d6b7d09a12d31
Souvik Ray <Souvik@sharethis.com>
2014-01-03 14:38:55 -0800

    changed

--- HG/HGAppDelegate.h
+++ HG/HGAppDelegate.h
@@ -9,6 +9,8 @@
 #import <UIKit/UIKit.h>
 #import <FacebookSDK/FacebookSDK.h>
 
+//modified
+
 @interface HGAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
